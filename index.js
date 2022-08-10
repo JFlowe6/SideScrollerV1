48fd2ef3ded2117942d013aab678b2a68dd07f18
Jack Lowe <jacklowe@live.com>
2022-08-10 02:09:51 +0100

    Messing around with the script tag in html

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import player from './img/player.png'
+import player1 from './img/player.png'
+console.log(player)
 const canvas = document.querySelector('canvas')
 
 const c = canvas.getContext('2d')
@@ -55,6 +56,9 @@ class Platform {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
+
+const image = new Image()
+image.src = player1
 
 const player = new Player()
 const platforms = [new Platform({x: 200, y: 100}), 
